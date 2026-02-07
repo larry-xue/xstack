cd524646c2fb40c7f2877fbf0926411e3d5ee8f0
薛宇建 <azoux0311@gmail.com>
2026-02-07 11:22:10 +0800

    feat: Initialize the `marketing` Astro application and define monorepo packages.

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,1 +1,16 @@
-export {}
+import { initContract } from '@ts-rest/core'
+import { z } from 'zod'
+
+const c = initContract()
+
+export const contract = c.router({
+  health: {
+    method: 'GET',
+    path: '/health',
+    responses: {
+      200: z.object({
+        ok: z.literal(true),
+      }),
+    },
+  },
+})
